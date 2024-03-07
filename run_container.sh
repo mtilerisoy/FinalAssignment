2aab7a51b327cb201ea5da8f3820a5fbde5049ce
mtilerisoy <mtilerisoy@gmail.com>
2024-03-07 18:24:56 +0100

    add new args

--- run_container.sh
+++ run_container.sh
@@ -6,4 +6,8 @@ export WANDB_START_METHOD="thread"
 wandb login
 
 torchrun --nnodes=1 --nproc_per_node=1 train.py \
-         --data_path "/gpfs/work5/0/jhstue005/JHS_data/CityScapes"
+         --data_path "/gpfs/work5/0/jhstue005/JHS_data/CityScapes" \
+         --model_version "1" \
+         --device "cuda" \
+         --learning_rate 0.01 \
+         --epochs 50
