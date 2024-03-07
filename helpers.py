36329930266a05bdec05f04c3446ae3d824fb3bf
mtilerisoy <mtilerisoy@gmail.com>
2024-03-07 16:30:17 +0100

    create visualization helper func

--- helpers.py
+++ helpers.py
@@ -1,3 +1,6 @@
+import matplotlib.pyplot as plt
+
+
 def print_dataset_info(dataset, filename='output.txt'):
     """
     Prints information about the dataset to a file.
@@ -30,3 +33,38 @@ def print_dataset_info(dataset, filename='output.txt'):
 
         print(f"Min and Max of scaled image:            {img255.min()}, {img255.max()}", file=f)
         print(f"Min and Max of scaled semantic seg:     {smnt255.min()}, {smnt255.max()}", file=f)
+
+
+def visualize_dataset(dataset, num_pairs=4):
+    """
+    Visualizes and SAVE a few images and their corresponding segmentation maps from the dataset.
+
+    Parameters:
+                dataset: Pytorch Dataset object
+                num_pairs: int  (default: 4)
+
+    Returns:
+                None
+    """
+    for i in range(num_pairs):
+        image, label = dataset[i]
+        
+        # PyTorch tensors for images usually have the channels dimension first,
+        # but matplotlib expects the channels dimension last.
+        # So, we need to rearrange the dimensions using permute.
+        image = image.permute(1, 2, 0)
+        label = label.permute(1, 2, 0)
+        
+        fig, ax = plt.subplots(1, 2, figsize=(10, 5))  # Adjust the size as needed
+        ax[0].imshow(image)
+        ax[0].set_title('Image')
+        
+        # Assuming the label is a PyTorch tensor, we need to convert it to numpy
+        label = label.numpy()
+        ax[1].imshow(label)
+        ax[1].set_title('Label')
+
+        # Save the figure
+        plt.savefig(f'visualization/image_and_label_{i}.png')
+        
+        plt.show()  
