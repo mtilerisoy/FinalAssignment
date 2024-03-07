88a1ee216db404bb2e82d856780a71da82be856e
mtilerisoy <mtilerisoy@gmail.com>
2024-03-07 11:42:03 +0100

    training simulation wih wandb

--- train.py
+++ train.py
@@ -8,6 +8,7 @@ import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
 from argparse import ArgumentParser
 import wandb
+import random
 
 # start a new wandb run to track this script
 wandb.init(
@@ -89,6 +90,19 @@ def main(args):
 
 
     # training/validation loop
+
+    # simulate a training
+    epochs = 10
+    offset = random.random() / 5
+    for epoch in range(2, epochs):
+        acc = 1 - 2 ** -epoch - random.random() / epoch - offset
+        loss = 2 ** -epoch + random.random() / epoch + offset
+        
+        # log metrics to wandb
+        wandb.log({"acc": acc, "loss": loss})
+        
+    # [optional] finish the wandb run, necessary in notebooks
+    wandb.finish()
 
 
     # save model
