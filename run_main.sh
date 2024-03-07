004c51b7fa01a020b2e841ffb75ac05b931e05e5
mtilerisoy <mtilerisoy@gmail.com>
2024-03-07 11:03:15 +0100

    run_main.sh home path config

--- run_main.sh
+++ run_main.sh
@@ -4,9 +4,9 @@
 #SBATCH --cpus-per-task=18
 #SBATCH --gpus=1
 #SBATCH --partition=gpu
-#SBATCH --time=1:00:00
+#SBATCH --time=00:02:00
 
-cd /gpfs/home4/<username>/FinalAssignment
+cd /gpfs/home4/scur0765/FinalAssignment
 mkdir wandb/$SLURM_JOBID
 
 srun apptainer exec --nv /gpfs/work5/0/jhstue005/JHS_data/5lsm0_v1.sif /bin/bash run_container.sh
