85df15fbdc99a4d7d2fb52cd50c0ba576a12fe36
mtilerisoy <mtilerisoy@gmail.com>
2024-03-13 15:37:39 +0100

    fix long squeeze in criterion

--- train.py
+++ train.py
@@ -98,7 +98,7 @@ def main(args):
             outputs = model(inputs)
             masks = (masks * 255).long().squeeze()
             masks = utils.map_id_to_train_id(masks).to(args.device)
-            loss = criterion(outputs, masks.long().squeeze())
+            loss = criterion(outputs, masks)
             loss.backward()
             optimizer.step()
 
